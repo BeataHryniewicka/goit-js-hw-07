58980c6cf62cacf7b4e3dd8302153e4f4a1623aa
BeataHryniewicka <118068394+BeataHryniewicka@users.noreply.github.com>
2023-03-29 23:54:35 +0200

    .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryArray = document.querySelector(`.gallery`);
+
+const newImages = galleryItems
+  .map(
+    (image) =>
+      //Link do oryginalnego obrazka powinien być przechowywany w atrybucie data source w elemencie <img>, oraz w href linku.
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</li>`
+  )
+  .join(" ");
+
+galleryArray.insertAdjacentHTML("beforeend", newImages);
+
+// galleryArray.addEventListener("click", (ev) => {
+//   //zablokowanie przekierowania na stronę
+//   ev.preventDefault();
+//   //Otworzenie okna modalnego po kliknięciu w element galerii.
+//   basicLightbox.create(`<img src="${ev.target.dataset.source}  ">`).show();
+// });
 
 console.log(galleryItems);
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryArray = document.querySelector(`.gallery`);
+
+const newImages = galleryItems
+  .map(
+    (image) =>
+      //Link do oryginalnego obrazka powinien być przechowywany w atrybucie data source w elemencie <img>, oraz w href linku.
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+   
+      alt="${image.description}"
+    />
+  </a>
+</li>`
+  )
+  .join(" ");
+
+galleryArray.insertAdjacentHTML("beforeend", newImages);
 
 console.log(galleryItems);
